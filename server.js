fcc4aff5be8a376bfd43a8ee93ac392821f3a45f
NancyChan <fanglanchan@gmail.com>
2018-03-25 20:29:15 +1300

    graphql

--- server.js
+++ server.js
@@ -1,15 +1,49 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
+import { makeExecutableSchema } from 'graphql-tools'
+
 import user from './server/routes/user'
 
 const app = express()
 const port = process.env.PORT || 5000
+
+// fake data
+const books = [
+  {
+    title: "Harry Potter and the Sorcerer's stone",
+    author: 'J.K. Rowling'
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton'
+  }
+]
+
+// The GraphQL schema in string form
+const typeDefs = `
+  type Query { books: [Book] }
+  type Book { title: String, author: String }
+`
+
+// The resolvers
+const resolvers = {
+  Query: { books: () => books }
+}
+
+// Put together a schema
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+})
 
 app.use(bodyParser.json()) // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
 app.use(cookieParser())
 
 app.use('/api', user)
+app.use('/graphql', bodyParser.json(), graphqlExpress({schema}))
+app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}))
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
